61d5dd6ef904e4c59f7d0128314d46de090e3db5
Azril Achmad <azrilachmad4@gmail.com>
2024-07-28 20:06:15 +0700

    initiap setup 1

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  corePlugins: {
+    preflight: false,
+  },
+  important: '#__next',
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
