d5be7dad2ad9309859f233f33c92856860e66f5e
Azril Achmad <azrilachmad4@gmail.com>
2024-07-31 12:15:16 +0700

    fix location is not defined

--- src/service/auth.js
+++ src/service/auth.js
@@ -2,26 +2,26 @@ import { setCookieValue } from "@/helpers";
 import axios from "axios";
 import { enqueueSnackbar } from "notistack";
 
-export const loginUser = async (params) => {
-  await axios.post(
-    `${process.env.NEXT_PUBLIC_API_URL}/${process.env.NEXT_PUBLIC_API_PREFIX}/${process.env.NEXT_PUBLIC_API_VERSION}/login`, JSON.stringify(params),
-    {
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-    }
-  ).then(response => {
-    if (response) {
-      // setCookieValue('cred_m', response.data.token)
-      // setCookieValue('user', response.data.nama)
-      localStorage.setItem('cred_m', response.data.token)
-      localStorage.setItem('user', response.data.nama)
-      localStorage.setItem('user_id', response.data.id)
-      window.location.href = '/dashboard'
-    }
-  })
-};
+// export const loginUser = async (params) => {
+//   await axios.post(
+//     `${process.env.NEXT_PUBLIC_API_URL}/${process.env.NEXT_PUBLIC_API_PREFIX}/${process.env.NEXT_PUBLIC_API_VERSION}/login`, JSON.stringify(params),
+//     {
+//       headers: {
+//         Accept: "application/json",
+//         "Content-Type": "application/json",
+//       },
+//     }
+//   ).then(response => {
+//     if (response) {
+//       // setCookieValue('cred_m', response.data.token)
+//       // setCookieValue('user', response.data.nama)
+//       localStorage.setItem('cred_m', response.data.token)
+//       localStorage.setItem('user', response.data.nama)
+//       localStorage.setItem('user_id', response.data.id)
+//       window.location.href = '/dashboard'
+//     }
+//   })
+// };
 
 export const logoutUser = async () => {
   const token = localStorage.getItem('cred_m')
@@ -42,7 +42,7 @@ export const logoutUser = async () => {
       // console.log(response)
       enqueueSnackbar("Successfully logout", {variant: 'success'})
       localStorage.clear()
-      window.location.href = '/'
+      // window.location.href = '/'
       return response
     }
   } catch (error) {
