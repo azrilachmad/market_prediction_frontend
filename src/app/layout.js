53fee9f50832054fe4720a21fdd289577e920b04
Azril Achmad <azrilachmad4@gmail.com>
2024-11-28 02:33:45 +0700

    Adjust message validation on protected route

--- src/app/layout.js
+++ src/app/layout.js
@@ -29,7 +29,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <title>POC PT.SIP</title>
+        <title>PT SIP</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="Market Prediction Apps" />
         <link rel="icon" href="/favicon.ico" />
@@ -39,7 +39,7 @@ export default function RootLayout({ children }) {
           <ThemeProvider theme={lightTheme}>
             <body id="__next">
               <CssBaseline />
-                <Headers pageProps={children} />
+              <Headers pageProps={children} />
             </body>
           </ThemeProvider>
         </SnackbarProvider>
