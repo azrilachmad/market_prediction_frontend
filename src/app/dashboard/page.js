d7ce3d827394984949c2b0af6633c8f8d828cd9f
Azril Achmad <azrilachmad4@gmail.com>
2025-01-10 03:09:37 +0700

    Implement API Card Data on Dashboard

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,6 +1,6 @@
 "use client"
 
-import { Grid, Icon, Paper, Typography } from "@mui/material"
+import { Button, Grid, Icon, Paper, ThemeProvider, Typography } from "@mui/material"
 import BarChartIcon from '@mui/icons-material/BarChart';
 import SubdirectoryArrowLeftIcon from '@mui/icons-material/SubdirectoryArrowLeft';
 import SubdirectoryArrowRightIcon from '@mui/icons-material/SubdirectoryArrowRight';
@@ -12,77 +12,88 @@ import { axisClasses } from '@mui/x-charts/ChartsAxis';
 import { getVehicleAssetData, getVehicleOmset, getVehicleSalesData, getVehicleType, getVehicleTypeCountData } from "@/service/marketPrediction";
 import { useQuery } from "react-query";
 import { dateTimePickerTabsClasses } from "@mui/x-date-pickers";
-import { removeDuplicateData, transformDataseries, transformDataset } from "@/helpers";
-import { getCard1Data } from "@/service/dashboard";
-import { PendingActions } from "@mui/icons-material";
+import { convDate, removeDuplicateData, transformDataseries, transformDataset } from "@/helpers";
+import { getCard1Data, getCard2Data, getCard3Data, getScheduleLog } from "@/service/dashboard";
+import { Edit, PendingActions } from "@mui/icons-material";
+import { useEffect, useState } from "react";
+import { MButton, YMDatePicker } from "@/components/form";
+import dayjs from "dayjs";
+import { primaryButton, secondaryButton } from "@/themes/theme";
+import { closeBtn } from "@/styles/theme/theme";
 export default function Dashboard() {
-
   const addCommas = (num) => num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
   const removeNonNumeric = (num) => num.toString().replace(/[^0-9]/g, "");
   const thousandSeparator = (number) => {
     return addCommas(removeNonNumeric(number))
   }
 
-  const {
-    data: card1Data,
-    isLoading: isLoadingCard1Data,
-  } = useQuery({
+
+  const [filterData, setFilterData] = useState({
+    startDate: new Date(Date.now() - 7 * 24 * 60 * 60 * 1000), // 7 days before today
+    endDate: new Date(), // Today
+  })
+
+  const initialFilterData = {
+    startDate: new Date(Date.now() - 7 * 24 * 60 * 60 * 1000), // 7 days before today
+    endDate: new Date(), // Today
+  }
+
+  console.log(filterData)
+
+  const handleInputDateChange = (event, name) => {
+    setFilterData({ ...filterData, [name]: event.$d })
+  }
+
+  const { data: card1Data, isLoading: isLoadingCard1Data, } = useQuery({
     queryKey: [
-      "vehicle-asset",
+      "card-data1",
       {
       },
     ],
     queryFn: ({ queryKey }) => getCard1Data(queryKey[1]),
   });
 
-  const {
-    data: card2Data,
-    isLoading: isLoadingCard2Data,
-  } = useQuery({
+  const { data: card2Data, isLoading: isLoadingCard2Data, } = useQuery({
     queryKey: [
-      "vehicle-type-count",
+      "card-data2",
       {
       },
     ],
-    queryFn: ({ queryKey }) => getVehicleTypeCountData(queryKey[1]),
+    queryFn: ({ queryKey }) => getCard2Data(queryKey[1]),
   });
 
-  const {
-    data: card3Data,
-    isLoading: isLoadingCard3Data,
-  } = useQuery({
+  const { data: card3Data, isLoading: isLoadingCard3Data, } = useQuery({
     queryKey: [
-      "omset",
+      "card-data3",
       {
       },
     ],
-    queryFn: ({ queryKey }) => getVehicleOmset(queryKey[1]),
+    queryFn: ({ queryKey }) => getCard3Data(queryKey[1]),
   });
 
-  const {
-    data: vehicleTypeData,
-    isLoading: isLoadingVehicleTypeData,
-    refetch: mutateVehicleTypeData,
-  } = useQuery({
-    queryKey: [
-      "vehicle-data",
-      {
-      },
-    ],
-    queryFn: ({ queryKey }) => getVehicleType(queryKey[1]),
-  });
-
-  const {
-    data: vehicleSalesData,
-    refetch: mutateVehicleSalesData,
-  } = useQuery({
+  const { data: logData, loading: isLoadingLogData, refetch: mutateLogData, } = useQuery({
     queryKey: [
       "vehicle-sales-data",
       {
+        ...filterData.startDate && { startDate: convDate(filterData.startDate, 'YYYY-MM-DD') },
+        ...filterData.endDate && { endDate: convDate(filterData.endDate, 'YYYY-MM-DD') },
       },
     ],
-    queryFn: ({ queryKey }) => getVehicleSalesData(queryKey[1]),
+
+    queryFn: ({ queryKey }) => getScheduleLog(queryKey[1]),
   });
+
+
+  const [chartData, setChartData] = useState([])
+
+  useEffect(() => {
+    if (logData?.data.length) {
+      setChartData(logData?.data)
+    }
+  }, [logData, setChartData])
+
+
+
 
   const renderCardList = () => {
     return (
@@ -108,7 +119,7 @@ export default function Dashboard() {
                   <Icon className="text-white text-[48px]" component={PendingActions} />
                 </div>
                 <div className="p-6">
-                  <Typography className="text-[20px]"><b>{card2Data?.data ? `${thousandSeparator(card2Data?.data)} Jenis` : '0'}</b></Typography>
+                  <Typography className="text-[20px]"><b>{thousandSeparator(card2Data?.data ? card2Data?.data : 0)} Unit</b></Typography>
                   <Typography className="text-[14px]">To be processed</Typography>
                 </div>
               </div>
@@ -121,7 +132,7 @@ export default function Dashboard() {
                   <Icon className="text-white text-[48px]" component={SubdirectoryArrowRightIcon} />
                 </div>
                 <div className="p-6">
-                  <Typography className="text-[20px]"><b>{card3Data?.data ? `Rp${thousandSeparator(card3Data?.data)}` : '0'}</b></Typography>
+                  <Typography className="text-[20px]"><b>{thousandSeparator(card3Data?.data ? card3Data?.data : 0)} Unit</b></Typography>
                   <Typography className="text-[14px]">Processed Data</Typography>
                 </div>
               </div>
@@ -132,130 +143,158 @@ export default function Dashboard() {
     )
   }
 
-
-  const renderChartOne = () => {
-
-    let datasets = []
-    vehicleSalesData?.data.map((data) => {
-      datasets.push({
-        [data.jenismobil]: data.harga
-      })
-    })
-    let data = []
-    if (vehicleTypeData?.data) {
-      vehicleTypeData?.data.map((val) => {
-        data.push({ value: val.value * 1, label: val.jenismobil })
-      })
-    }
-
-    const chartSetting = {
-      yAxis: [
-        {
-          label: 'Penjualan',
-        },
-      ],
-      height: 460,
-      sx: {
-        [`.${axisClasses.left} .${axisClasses.label}`]: {
-          transform: 'translate(-20px, 0)',
-        },
-      },
-    };
-
-    const valueFormatter = (value) => `${value}`;
-
-    let dataSeries = []
-    if (vehicleSalesData?.data) {
-      vehicleSalesData?.data
-    }
+  const renderFilterPaper = () => {
     return (
       <>
-        <Grid container spacing={2} className="mt-8">
+        <Paper variant="outlined" className="w-[700px] rounded-[8px] p-4 mt-12">
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Typography className="font-bold mb-4">Filter:</Typography>
+              <div className="flex items-center">
+                <YMDatePicker
+                  className='mr-4'
+                  fullwidth={true}
+                  name="startDate"
+                  label="Start Date"
+                  inputFormat={'DD-MM-YYYY'}
+                  onChange={(event) => handleInputDateChange(event, 'startDate')}
+                  value={dayjs(filterData.startDate)}
+                  closeOnSelect={true}
+                // errorMessage={errorMessage && errorMessage?.startDate ? errorMessage?.startDate[0] : false}
+                />
+                <YMDatePicker
+                  className='mr-4'
+                  fullwidth={true}
+                  name="endDate"
+                  label="End Date"
+                  inputFormat={'DD-MM-YYYY'}
+                  onChange={(event) => handleInputDateChange(event, 'endDate')}
+                  value={dayjs(filterData.endDate)}
+                  closeOnSelect={true}
+                // errorMessage={errorMessage && errorMessage?.endDate ? errorMessage?.endDate[0] : false}
+                />
+                <ThemeProvider theme={primaryButton}>
+                  <MButton
+                    className='mr-4'
+                    label="Apply"
+                    onClick={() => {
+                      mutateLogData();
+                    }}
+                  />
+                </ThemeProvider>
+                <ThemeProvider theme={secondaryButton}>
+                  <MButton
+                    label="Reset"
+                    onClick={() => {
+                      setFilterData(initialFilterData)
+                      mutateLogData()
+                    }}
+                  />
+                </ThemeProvider>
+              </div>
+            </Grid>
+          </Grid>
+        </Paper>
+      </>
+    )
+  }
+
+  const renderChartOne = () => {
+    const vehicleSalesData = []
+
+    console.log(chartData)
+
+    const xAxisData = chartData?.map((item) =>
+      new Date(item.date)
+    );
+    const totalToken = chartData?.map((item) => item.total_token);
+    const averageToken = chartData?.map((item) => item.average_token);
+    
+    const totalData = chartData?.map((item) => item.total_data);
+    const totalDuration = chartData?.map((item) => item.duration);
+
+
+
+    return (
+      <>
+        <Grid container spacing={2} className="mt-2">
           <Grid item xs={6}>
             <Paper variant="outlined" className="rounded-[10px] py-6">
-              <div className="flex justify-center mb-6">
-                <Typography className="text-[20px] font-bold">Top Sales Performance by Car Type
+              <div className="flex justify-center ">
+                <Typography className="text-[20px] font-bold mb-4">AI Token Usage per Schedule
                 </Typography>
               </div>
               <div>
                 {/* Bar Chart */}
-                <BarChart
-                  className="py-8 ml-4"
-                  dataset={transformDataset(vehicleSalesData?.data)}
-                  xAxis={[{ scaleType: 'band', dataKey: 'month' }]}
-                  series={transformDataseries(vehicleSalesData?.data)}
-                  {...chartSetting}
+                <LineChart
+                  xAxis={[
+                    {
+                      label: 'Date',
+                      data: xAxisData,
+                      tickInterval: xAxisData,
+                      scaleType: "time",
+                      valueFormatter: (date) => dayjs(date).format("DD/MM/YYYY"),
+                    },
+                  ]}
+                  series={[
+                    {
+                      id: 'series-1',
+                      label: 'Total Token',
+                      data: totalToken ? totalToken : [],
+                      valueFormatter: (value) => thousandSeparator(value),
+                    },
+                    {
+                      id: 'series-2',
+                      label: 'Average Token per hit',
+                      data: averageToken ? averageToken : [],
+                      valueFormatter: (value) => (value),
+
+                    },
+                  ]}
+
+                  // width={'auto'}
+                  height={400}
                 />
               </div>
             </Paper>
           </Grid>
           <Grid item xs={6}>
-            <Paper variant="outlined" className="rounded-[10px] pt-4 pb-10 pl-4 pr-4 h-[565px] items-center">
-              <div className="flex justify-center mb-6">
-                <Typography className="text-[20px] font-bold">Top 10 Best Selling Cars 2023</Typography>
-              </div>
-              <div style={{ marginLeft: 24}}>
-                <PieChart
-                className="mt-24"
-                  series={[
-                    {
-                      data,
-                      highlightScope: { faded: 'global', highlighted: 'item' },
-                      faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
-                    },
-                  ]}
-                  height={338}
-                />
-              </div>
-            </Paper>
-          </Grid>
-        </Grid>
-      </>
-    )
-  }
-  const renderChartTwo = () => {
-    const hi_data = [180000000, 178000000, 165000000, 176000000, 179000000, 180000000, 181000000, 180000000, 180000000, 185000000, 200000000, 185000000];
-    const low_data = [160000000, 158000000, 156000000, 157000000, 160000000, 160000000, 162000000, 162000000, 165000000, 165000000, 170000000, 165000000];
-    const sold_data = [170000000, 165000000, 150000000, 168000000, 168000000, 170000000, 175000000, 175000000, 175000000, 178000000, 190000000, 180000000];
-    const months = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December"
-    ];
-
-    return (
-      <>
-        <Grid container spacing={2} className="mt-8">
-          <Grid item xs={12}>
-            <Paper variant="outlined" className="rounded-[10px] p-4">
-              <div className="flex justify-center mb-6">
-                <Typography className="text-[32px] font-bold">Sales Data</Typography>
-              </div>
-              <div className="ml-4">
-                <Typography>Mobil: <b>Toyota Avanza G MT</b></Typography> 
-              </div>
-              <div className="ml-4">
-                <Typography>Tahun: <b>2023</b></Typography> 
+            <Paper variant="outlined" className="rounded-[10px] py-6">
+              <div className="flex justify-center ">
+                <Typography className="text-[20px] font-bold mb-4">Time Taken by Scheduler Processes</Typography>
               </div>
               <div>
+                {/* Bar Chart */}
                 <LineChart
-                className="p-2"
-                  height={400}
-                  series={[
-                    { data: low_data, label: "Market Top Price" },
-                    { data: hi_data, label: "Market Bottom Price" },
-                    { data: sold_data, label: "Actual Sold Price" }
+                  xAxis={[
+                    {
+                      label: 'Date',
+                      data: xAxisData,
+                      tickInterval: xAxisData,
+                      scaleType: "time",
+                      valueFormatter: (date) => dayjs(date).format("DD/MM/YYYY"),
+                    },
                   ]}
-                  xAxis={[{ scaleType: "point", data: months }]}
+                  series={[
+                    {
+                      id: 'series-1',
+                      label: 'Total Data',
+                      data: totalData ? totalData : [],
+                      valueFormatter: (value) => `${value} Data`,
+                      color: 'purple'
+                    },
+                    {
+                      id: 'series-2',
+                      label: 'Duration (s)',
+                      data: totalDuration ? totalDuration : [],
+                      valueFormatter: (value) => `${value} Seconds`,
+                      color: 'blue',
+
+                    },
+                  ]}
+
+                  // width={'auto'}
+                  height={400}
                 />
               </div>
             </Paper>
@@ -264,13 +303,14 @@ export default function Dashboard() {
       </>
     )
   }
+
 
   return (
     <>
       <div>
         {renderCardList()}
+        {renderFilterPaper()}
         {renderChartOne()}
-        {renderChartTwo()}
       </div>
     </>
   )
