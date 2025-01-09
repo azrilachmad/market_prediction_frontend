c40134b516cd7249c300b77dd12b18320ee61be5
Azril Achmad <azrilachmad4@gmail.com>
2025-01-09 19:52:42 +0700

    Implement To Be processed data API to web UI

---
+++ src/service/dashboard.js
@@ -0,0 +1,79 @@
+'use client'
+
+import { convDate, getCookieValueByKey } from "@/helpers";
+import axios from "axios";
+import { enqueueSnackbar } from "notistack";
+const token = getCookieValueByKey("token");
+
+
+export const getCard1Data = async (params) => {
+  const config = {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      'Authorization': `Bearer ${token}`
+    },
+    params: {
+    }
+  }
+
+  try {
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/dashboard/card1`, config
+    );
+    if (response) {
+      return response.data
+    }
+  } catch (error) {
+
+  }
+
+};
+
+export const getCard2Data = async (params) => {
+  const config = {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      'Authorization': `Bearer ${token}`
+    },
+    params: {
+    }
+  }
+
+  try {
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/dashboard/card2`, config
+    );
+    if (response) {
+      return response.data
+    }
+  } catch (error) {
+
+  }
+
+};
+
+export const getCard3Data = async (params) => {
+  const config = {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      'Authorization': `Bearer ${token}`
+    },
+    params: {
+    }
+  }
+
+  try {
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/dashboard/card3`, config
+    );
+    if (response) {
+      return response.data
+    }
+  } catch (error) {
+
+  }
+
+};
